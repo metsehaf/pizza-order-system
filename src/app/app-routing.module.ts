3987f1c162d009a286430474e0c2144752ab7626
Girum Hagos <hagosgirum@gmail.com>
2019-07-17 22:56:44 -0400

    bootstrapped routing for application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OrderPizzaComponent } from './order-pizza/order-pizza.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'order',
+    component: OrderPizzaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
