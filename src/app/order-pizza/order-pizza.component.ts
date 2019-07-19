1f1a43765cf35117d024a6cc6d391fa5c06f3347
Girum Hagos <hagosgirum@gmail.com>
2019-07-18 20:27:47 -0400

    Created four array of strings / number for pizza size

--- src/app/order-pizza/order-pizza.component.ts
+++ src/app/order-pizza/order-pizza.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-pizza.component.css']
 })
 export class OrderPizzaComponent implements OnInit {
-
+  smallPizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
+  mediumPizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
+  largePizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
+  extraLargePizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
   constructor() { }
 
   ngOnInit() {
