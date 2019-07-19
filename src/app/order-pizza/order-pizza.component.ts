e9782f05387a30c297dc64d1bac8efb785a51c2f
Girum Hagos <hagosgirum@gmail.com>
2019-07-18 20:40:50 -0400

    Imported Form builder and reactive forms module

--- src/app/order-pizza/order-pizza.component.ts
+++ src/app/order-pizza/order-pizza.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-order-pizza',
@@ -6,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-pizza.component.css']
 })
 export class OrderPizzaComponent implements OnInit {
-  smallPizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
-  mediumPizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
-  largePizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
-  extraLargePizza: Array<string | number> = [1, 0.50, 1, 1.20, 0.75, 1, 2, 3];
-  constructor() { }
+  smallPizza: Array<string | number> = ['Tomatoes - ( $1.00)',
+              'Onions - ( $0.50)', 'Bell pepper - ( $1.00)',
+              'Mushrooms – ($1.20)', 'Pineapple – ($0.75)',
+              'Sausage ($1.00)', 'Pepperoni ($2.00)', 'Barbecue chicken ($3.00)'];
+  mediumPizza: Array<string | number> = ['Tomatoes - ( $1.00)',
+              'Onions - ( $0.50)', 'Bell pepper - ( $1.00)',
+              'Mushrooms – ($1.20)', 'Pineapple – ($0.75)',
+              'Sausage ($1.00)', 'Pepperoni ($2.00)', 'Barbecue chicken ($3.00)'];
+  largePizza: Array<string | number> = ['Tomatoes - ( $1.00)',
+              'Onions - ( $0.50)', 'Bell pepper - ( $1.00)',
+              'Mushrooms – ($1.20)', 'Pineapple – ($0.75)',
+              'Sausage ($1.00)', 'Pepperoni ($2.00)', 'Barbecue chicken ($3.00)'];
+  extraLargePizza: Array<string | number> = ['Tomatoes - ( $1.00)',
+              'Onions - ( $0.50)', 'Bell pepper - ( $1.00)',
+              'Mushrooms – ($1.20)', 'Pineapple – ($0.75)',
+              'Sausage ($1.00)', 'Pepperoni ($2.00)', 'Barbecue chicken ($3.00)'];
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
